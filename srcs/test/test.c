c397faa29f885c035e7a3aa3b9f5cf8a44102ead
Theau De La Ruelle <tde-la-r@2G1.42angouleme.fr>
2024-03-15 19:11:44 +0100

    rearrenging tests

--- srcs/test/test.c
+++ srcs/test/test.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
+#include <termios.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void    error(char *err)
-{
-        perror(err);
-        exit (1);
+struct termios saved;
+
+void restore(void) {
+    tcsetattr(STDIN_FILENO, TCSANOW, &saved);
 }
 
-int main (void)
-{
-        char *cwd = getcwd(NULL, 0);
-        unlink(cwd);
-        error("getcwd");
-}       
+
+int main() {
+    struct termios attributes;
+
+    tcgetattr(STDIN_FILENO, &saved);
+    atexit(restore);
+
+    tcgetattr(STDIN_FILENO, &attributes);
+    attributes.c_lflag &= ~ ECHO;
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &attributes);
+
+    printf("Entering the loop\n");
+    while(1) {};
+}
