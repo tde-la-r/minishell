f1de1bb21063e2446f39e2a4d863d1a59b53b529
Theau De La Ruelle <tde-la-r@2G4.42angouleme.fr>
2024-04-19 15:21:20 +0200

    1try

--- srcs/test/t_ifs.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int	main()
-{
-	char *ifs = getenv("IFS");
-	printf("%s", ifs);
-}
--- srcs/test/t_pwd.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int	main(void)
-{
-	printf("%s", getcwd(NULL, 0));
-}
